fe8ec113c9e3e307eaf614ede27bc4e994600713
Achiever-P <202351103@iiitvadodara.ac.in>
2025-07-19 07:30:38 +0530

    deloy try

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import Cta from "@/components/Cta";
@@ -9,31 +11,30 @@ const Page = async () => {
     const companions = await getAllCompanions({ limit: 3 });
     const recentSessionsCompanions = await getRecentSessions(10);
 
-  return (
-    <main>
-      <h1>Dashboard</h1>
+    return (
+        <main>
+            <h1>Dashboard</h1>
 
-        <section className="home-section">
-            {companions.map((companion) => (
-                <CompanionCard
-                    key={companion.id}
-                    {...companion}
-                    color={getSubjectColor(companion.subject)}
+            <section className="home-section">
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+            </section>
+
+            <section className="home-section">
+                <CompanionsList
+                    title="Recently completed sessions"
+                    companions={recentSessionsCompanions}
+                    classNames="w-2/3 max-lg:w-full"
                 />
-            ))}
+                <Cta />
+            </section>
+        </main>
+    );
+};
 
-        </section>
-
-        <section className="home-section">
-            <CompanionsList
-                title="Recently completed sessions"
-                companions={recentSessionsCompanions}
-                classNames="w-2/3 max-lg:w-full"
-            />
-            <Cta />
-        </section>
-    </main>
-  )
-}
-
-export default Page
+export default Page;
